05802d9dc9b729e2fe047339cd0c3b0c3c74fd9b
[tekesashia] <[t.yamashita059@gmail.com]>
2021-02-05 23:31:11 +0900

    implement

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const INCREMENT ='INCREMENT'
+export const DECREMENT ='DECREMENT'
+
+export const INCREMENT = () => ({
+        type: INCREMENT
+    })
+export const INCREMENT = () => ({
+    type: DECREMENT
+})
