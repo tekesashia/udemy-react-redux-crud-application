e17e34c8c8f07a48267bb9fd81cff5b3511e786e
[tekesashia] <[t.yamashita059@gmail.com]>
2021-02-06 00:17:30 +0900

    fin2

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
-export const INCREMENT ='INCREMENT'
-export const DECREMENT ='DECREMENT'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
 
-export const INCREMENT = () => ({
-        type: INCREMENT
-    })
-export const INCREMENT = () => ({
-    type: DECREMENT
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
 })
--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import './index.css';
-import reducer from "./reducers";
+import reducer from './reducers'
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer)
 
 ReactDOM.render(
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>,
-document.getElementById('root')
+  </Provider>,
+  document.getElementById('root')
 );
-
 registerServiceWorker();
